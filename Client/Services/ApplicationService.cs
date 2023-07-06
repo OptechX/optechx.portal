ee3f2c1680683711518f764275aa2c118327ac39
Danijel-James Wynyard <danijel@repasscloud.com>
2023-07-07 00:42:27 +1000

    clean legacy code

--- Client/Services/ApplicationService.cs
+++ Client/Services/ApplicationService.cs
@@ -24,31 +24,6 @@ namespace OptechX.Portal.Client.Services
                 {
                     Applications = response;
                 }
-                //Applications = await _httpClient.GetFromJsonAsync<IList<Application>>("api/Application") ?? new List<Application>
-                //{
-                //    new Application
-                //    {
-                //        Id = 0,
-                //        UID = "Google::Chrome",
-                //        LastUpdate = DateTime.UtcNow,
-                //        ApplicationCategory = ApplicationCategory.INTERNET,
-                //        Publisher = "Fish",
-                //        Name = "Fingers",
-                //        Version = "114.0.5735.199",
-                //        Copyright = "Copyright 2023 Google LLC. All rights reserved.",
-                //        LicenseAcceptRequired = false,
-                //        Lcid = new string[] { "x64","x86" },
-                //        CpuArch = new string[] { "EN_US" },
-                //        Homepage = "https://www.google.com/chrome/browser/",
-                //        Icon = "https://raw.githubusercontent.com/OptechX/library.apps.images/main/Internet/Google/Chrome/icon.svg",
-                //        Docs = "https://support.google.com/chrome/?hl=en&rd=3#topic=7439538",
-                //        License = "https://www.google.it/intl/en/chrome/browser/privacy/eula_text.html",
-                //        Tags = new string[] { "google","chrome","internet","browser" },
-                //        Summary = "Chrome is a fast, simple, and secure web browser, built for the modern web.",
-                //        Enabled = true,
-                //        BannerIcon = null,
-                //    }
-                //};
             }
         }
     }
