08b746337e96fe4e913e833252fdab09db8d54ff
Danijel-James Wynyard <danijel@repasscloud.com>
2023-07-09 14:45:37 +1000

    fixes #20

--- Shared/Models/User/UserLogin.cs
+++ Shared/Models/User/UserLogin.cs
@@ -7,7 +7,7 @@ namespace OptechX.Portal.Shared.Models.User
         [Required(ErrorMessage = "Valid email address requried"), EmailAddress]
         public string Email { get; set; } = null!;
 
-        [StringLength(100, MinimumLength = 16, ErrorMessage = "Password length between 16-100 characters")]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "Password length between 8-32 characters")]
         public string Password { get; set; } = null!;
     }
 }
